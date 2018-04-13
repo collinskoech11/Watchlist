e22d7829accec0c2aef3778e8e8754634f3818ef
ramza007 <ramzalvo007@gmail.com>
2018-04-13 16:19:24 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -2,19 +2,39 @@ import os
 
 class Config:
 
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://razma:ramza123@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 class ProdConfig(Config):
     pass
 
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ramza:ramza123@localhost/watchlist_test'
+
+
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ramza:ramza123@localhost/watchlist'
     DEBUG = True
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
+
+#  email configurations
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
--- manage.py
+++ manage.py
@@ -1,12 +1,18 @@
-from app import create_app
-from flask_script import Manager, Server
+from app import create_app, db
+from flask_script import Manager, Server, Shell
+from app.models import User, Role, Review
+from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
+app = create_app('test')
+
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
 manager.add_command('server', Server)
-manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
@@ -15,6 +21,11 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role, Review=Review)
 
 
 if __name__ == '__main__':

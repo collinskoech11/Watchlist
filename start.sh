1080c2afc626ecd65bd5437249fee73d13c682d6
Ramza <36161184+ramza007@users.noreply.github.com>
2018-08-13 17:29:21 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export MOVIE_API_KEY='50a5e60b5b64655cb7e424b18eb461f5'
-export SECRET_KEY='ramza1235'
-export MAIL_USERNAME='ramzalvo007@gmail.com'
-export MAIL_PASSWORD='rAMSARESENI14'
-
-python3.6 manage.py server
